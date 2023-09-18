082dde301c8799449b5699041501ed7353509e2d
Makarov_Stanislav <makarov1003@mail.ru>
2023-09-18 14:58:40 +0300

    Run second site

--- GoodSite/people/views.py
+++ GoodSite/people/views.py
@@ -4,5 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse('Страница приложения для Людей')
+
+
 def about(request):
     return HttpResponse('<h1> БГИТУ </h1>')
+
+
+def pri_id(request, number_student):
+    return HttpResponse(f'<h1> ПрИ-201 </h1> <p>Студент под номером {number_student} </p>')
