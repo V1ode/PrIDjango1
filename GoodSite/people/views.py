7720cf70730c75af0aebee6756c8842d5ab52122
Kovac <gf.356@yandex.ru>
2023-10-16 07:30:06 +0300

    Add DataBase class

--- GoodSite/people/views.py
+++ GoodSite/people/views.py
@@ -20,10 +20,41 @@ class data_handler:
             2024: 'Возможно, будет хорошо',
             2025: 'Будет хорошо'
         }
+        def return_date_info(self):
+            return self.dates[self.year]
 
 
-    def return_date_info(self):
-        return self.dates[self.year]
+class DataBase:
+    __instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
+
+    def __del__(self):
+        self.__instance = None
+
+    def __init__(self, user, psw, port):
+        self.user = user
+        self.psw = psw
+        self.port = port
+        self.data = "DATA"
+
+    def connect(self):
+        print(f"Соединение с Базой данных:{self.user}, "
+              f"{self.psw}, {self.port}")
+
+    def close(self):
+        print("Закрыть соединение с Базой данных")
+
+    def read(self):
+        return "Данные из Базы данных"
+
+    def write(self):
+        print(f"Запись в Базу данных {self.data}")
+
+
 
 
 pri_info = {
